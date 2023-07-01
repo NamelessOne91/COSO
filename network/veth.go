48b400d7038bd129d2e89c0b2c0b01fadeca90fc
Paolo Invernizzi <paolo.invernizzi@lightcast.com>
2023-07-01 16:44:12 +0200

    Add Virtual Ethernet description

--- network/veth.go
+++ network/veth.go
@@ -1,5 +1,10 @@
 package network
 
+// Veth represents a virtual Ethernet device pair. Veth devices are always created in interconnected pairs.
+//
+// Veth devices can act as a tunnel etween network namespaces to create a bridge to a
+// physical network device in another namespace. That's because packets transmitted on one device in the pair
+// are immediately received on the other device
 type Veth struct{}
 
 func NewVeth() *Veth {
